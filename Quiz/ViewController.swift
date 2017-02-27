d92eaba79e2ec0646d2d2eaebb5df30973324b2d
akirkman10 <akirkman@highpoint.edu>
2017-02-27 12:46:14 -0500

    Bronze & Silver Challenge

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Alexis Kirkman on 1/11/17.
 //  Copyright © 2017 Alexis Kirkman. All rights reserved.
-//
+//  Displays quiz questions and answers.
 
 import UIKit
 
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
    @IBOutlet var answerLabel: UILabel!
    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
+   var labelSpace = UILayoutGuide() // UI Layout Guide
+   var statusCount: Int = 1 // answer display count
    
    let questions: [String] = [
       "What is 7+7?",
@@ -30,6 +32,13 @@ class ViewController: UIViewController {
    override func viewDidLoad() {
       super.viewDidLoad()
       currentQuestionLabel.text = questions[currentQuestionIndex]
+      
+      //UILayoutGuide
+      self.view.addLayoutGuide(labelSpace)
+      labelSpace.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+      nextQuestionLabel.centerXAnchor.constraint(equalTo: labelSpace.leadingAnchor).isActive = true
+      currentQuestionLabel.centerXAnchor.constraint(equalTo: labelSpace.trailingAnchor).isActive = true
+
       updateOffScreenLabel()
    }
    func updateOffScreenLabel() {
@@ -50,10 +59,16 @@ class ViewController: UIViewController {
       animateLabelTransitions()
    }
    
-   
+   // Adjusts answer display. Doesn't show anwer when 
+   // statusCount % 2 doesn't equal 0
    @IBAction func showAnswer(_ sender: UIButton){
-      let answer: String = answers[currentQuestionIndex]
-      answerLabel.text = answer
+      if(statusCount%2  != 0) {
+         let answer: String = answers[currentQuestionIndex]
+         answerLabel.text = answer
+      }
+      else{
+        answerLabel.text = "???"
+      }
    }
    
    func animateLabelTransitions(){
@@ -62,28 +77,18 @@ class ViewController: UIViewController {
       
       // Animate the alpha
       // and the center X constraints
-      let screenWidth = view.frame.width
+      //let screenWidth = view.frame.width
       self.nextQuestionLabelCenterXConstraint.constant = 0
-      self.currentQuestionLabelCenterXConstraint.constant += screenWidth
-      
+      //self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+      statusCount = statusCount + 1
       UIView.animate( withDuration: 0.5,
                       delay: 0,
+                      usingSpringWithDamping: 0.5,//adds spring effect to questions
+                      initialSpringVelocity: 0.5,
                       options: [.curveLinear],
                       animations: {
                         self.currentQuestionLabel.alpha = 0
                         self.nextQuestionLabel.alpha = 1
-                        
-                        //self.view.layoutIfNeeded()
-      })
-      UIView.animate(withDuration: 0.5,
-                     delay: 0,
-                     usingSpringWithDamping: 0.5,
-                     initialSpringVelocity: 0.5,
-                     options: [],
-                     animations: {
-                        self.currentQuestionLabel.alpha = 0
-                        self.nextQuestionLabel.alpha = 1
-                                          
                         self.view.layoutIfNeeded()
                         
       }, completion: { _ in
@@ -98,7 +103,7 @@ class ViewController: UIViewController {
    }
    
    override func viewWillAppear(_ animated: Bool) {
-      super.viewWillAppear( animated)
+      super.viewWillAppear(animated)
       // Set the label's initial alpha
       nextQuestionLabel.alpha = 0
    }
